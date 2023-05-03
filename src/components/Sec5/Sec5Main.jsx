bec264072cec00b5b554a6ad69902cfb2c888028
jojo_nguyen <jnguyen@bgwgroup.com.au>
2023-05-03 14:21:13 +1000

    test

--- src/components/Sec5/Sec5Main.jsx
+++ src/components/Sec5/Sec5Main.jsx
@@ -4,6 +4,9 @@ import { Timeline } from './Timeline';
 export const Sec5Main = () => {
   return (
     <div className='sec5-main-ctn'>
+        <div className="sec5-main-heading-ctn">
+          <div className="sec5-main-heading">Our Timeline</div>
+        </div>
         <Timeline />
     </div>
   )
