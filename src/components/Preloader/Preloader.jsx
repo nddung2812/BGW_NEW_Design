1eb487b78e0c2a850c2765696ddf7e81b1d68584
jojo_nguyen <jnguyen@bgwgroup.com.au>
2023-05-03 13:55:28 +1000

    'test'

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react"
 import "./Preloader.css";
 import { preLoaderAnim } from "../../preloaderAnimations/preloaderAnimation";
-
+import bgwLogoImage from "../../assets/bgwLogo.png";
 export const Preloader = () => {
   const bgwLogo = "https://firebasestorage.googleapis.com/v0/b/bgwgroupwebapp.appspot.com/o/BusinessLogos%2FbgwLogo.png?alt=media&token=ae2ffe95-8fef-438d-b702-1a1d1c719c5a"
 
@@ -13,7 +13,8 @@ export const Preloader = () => {
   return (
   <div className="preloader">
       <div className="texts-container">
-          <span className="preload-img-ctn"><img src={bgwLogo}/></span>
+          {/* <span className="preload-img-ctn"><img src={bgwLogo}/></span> */}
+          <span className="preload-img-ctn"><img src={bgwLogoImage}/></span>
           <span>BGW </span>
           <span>GROUP<sup className="preload-trademark">™</sup></span>
       </div>
